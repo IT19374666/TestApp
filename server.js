30789ba567f6bee400b2a2e64ea48bf1d2c765cc
IT19374666 <68698512+IT19374666@users.noreply.github.com>
2021-09-29 15:46:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-// server.js
+//Install express server
 const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+const path = require('path');
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('../dist/googleplaystore-app-heroku'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: '../dist/googleplaystore-app-heroku/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 7890);
